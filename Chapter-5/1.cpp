f0bc6ae4d6c72be5ef073bf88d67291aac324ce0
WangYan-Good <987639823@qq.com>
2021-03-02 13:37:48 +0000

    5-1

--- Chapter-5/1.cpp
+++ Chapter-5/1.cpp
@@ -2,8 +2,23 @@
 #include<vector>
 using namespace std;
 /*
+编写一个要求用户输入两个整数的程序。该程序将计算并输出这两个整数之间（包括这两个整数）所有整数的和。这里假设先输入较小的整数。例如，如果用户输入的是2和9，则程序将指出2~9之间所有整数的和为44。
 */
 int main()
 {
+  int min;
+  int max;
+  int sum = 0;
+  cout << "Enter the smaller number:";
+  cin >> min;
+
+  cout << "Enter the larger number:";
+  cin >> max;
+  
+  for (int index=min; index <= max; index++ )
+  {
+    sum += index; 
+  }
+  cout << "the sum of those number is: " << sum << endl;
   return 0;
 }
---
+++ Chapter-5/2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<array>
+using namespace std;
+/*
+使用array对象（而不是数组）和long double（而不是long long）重新编写程序清单5.4，并计算100！的值。
+*/
+int main()
+{
+  array<double,101> arr;
+  arr[0] = 1;
+  for (int index=0; index < 100; index++)
+  {
+    arr[index+1] *= arr[index];
+    cout << index << "! =" << arr[index] << endl;
+  }
+    cout  << "100! =" << arr[100] << endl;
+  return 0;
+}
