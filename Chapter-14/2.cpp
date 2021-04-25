1592e3567a667490c409698d622d838f0d5ca182
WangYan-Good <987639823@qq.com>
2021-04-26 01:12:57 +0800

    C++_Chapter14/2

---
+++ Chapter-14/2.cpp
@@ -0,0 +1,59 @@
+#include "2.h"
+
+template <typename T1, typename T2>
+void Pair<T1, T2>::set( const T1 & yr, const T2 & bot )
+{
+
+}
+
+template <typename T1, typename T2>
+int Pair<T1,T2>::Sum() const
+{
+
+}
+
+template <typename T1, typename T2>
+void Pair<T1,T2>::Show(int y) const
+{
+
+} 
+
+Wine::Wine( const char* l, int y, const int yr[], const int bot[] )
+{
+
+}
+
+Wine::Wine( char* l, int y )
+{
+
+}
+
+void Wine::GetBottles()
+{
+
+}
+
+std::string & Wine::Label()
+{
+
+}
+
+int Wine::sum()
+{
+
+}
+
+void Wine::Show() const
+{
+
+}
+
+Wine & Wine::operator=( const Wine & wine )
+{
+
+}
+
+std::ostream & operator<<( std::ostream & os, const Wine & wine )
+{
+    
+}
