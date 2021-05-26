6f6ac4cda736d9247e6a3eb2cc965709a445d1cc
WangYan-Good <987639823@qq.com>
2021-05-27 00:43:27 +0800

    C++_Chapter15/2

--- Chapter-15/2.h
+++ Chapter-15/2.h
@@ -12,16 +12,26 @@ class bad_hmean : public std::logic_error
     double       v1;
     double       v2;
   public:
-    explicit bad_hmean( const std::string & name = "hmean", const std::string & s = "Error in hmean()\n", double a = 0, double b = 0 );
-
+    explicit bad_hmean( const std::string & n = "hmean", const std::string & s = "Error in hmean()\n", double a = 0, double b = 0 );
+    void mesg();
+    const char * what() { return "bad arguments in hmean()\n"; }
+    virtual ~bad_hmean() throw() {}
 };
 
-class bad_gmen : public std::logic_error
+class bad_gmean : public std::logic_error
 {
   private:
-
+    std::string name;
   public:
-
+    double v1;
+    double v2;
+    explicit bad_gmean( const std::string & name = "gmean", const std::string & s = "Error in gmean()\n", double a = 0, double b = 0 );
+    const char * mesg();
+    const char * what() { return "bad arguments in gmean()\n"; }
+    virtual ~bad_gmean() throw() {}
 };
+
+double hmean( double a, double b );
+double gmean( double a, double b );
 
 #endif
