65f618d23cc194b7c7a2211caa56d00beea3e255
Wang <yanx.wang@intel.com>
2021-06-09 15:48:31 +0800

    C++_Chapter16/5

--- Chapter-16/5-main.cpp
+++ Chapter-16/5-main.cpp
@@ -4,3 +4,22 @@ template<class T>
 int reduce(T ar[], int n):
 在一个使用long实例和string实例的小程序中测试该函数。
 */
+#include "5.h"
+using namespace std;
+int main()
+{
+  long testarr[10] = { 156, 124, 23, 43, 35, 23, 124, 199, 210, 156 };
+  reduce( testarr, 10 );
+  for( auto x:testarr )
+  {
+    std::cout << x <<std::endl;
+  }
+  std::cout << std::endl;
+  string strarr[10] = { "Hello", "Good", "Tomorrow", "Yesterday", "Hi" };
+  reduce( strarr, 10 );
+  for( auto x:strarr )
+  {
+    std::cout << x << std::endl;
+  }
+  return 0;
+}
--- Chapter-16/5.cpp
+++ Chapter-16/5.cpp
@@ -3,5 +3,7 @@
 template<class T>
 int reduce(T ar[], int n)
 {
-  
+  std::sort( &ar[0], &ar[n] );
+  T * new_ar = std::unique( &ar[0], &ar[n] );
+  return new_ar - ar;
 }
