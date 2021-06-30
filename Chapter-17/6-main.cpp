4850434f1a84b3f13432c2195f081ddc47c9284e
WangYan-Good <987639823@qq.com>
2021-06-30 23:08:02 +0800

    C++_Chapter17/6

--- Chapter-17/6-main.cpp
+++ Chapter-17/6-main.cpp
@@ -13,6 +13,31 @@ employee * pc[MAX];
 然后该对象可以使用虚函数 setall() 来提示用户输入相应的数据：
 pc[i]->setall(); // invokes function corresponding to type of object
 为将数据保存到文件中，应设计一个虚函数 writeall();
+for(i = 0; i < index; i++)
+    pc[i]->writeall(fout); // fout ofstream connected to output file
+注意:对于这个练习，应使用文本IO，而不是二进制IO(遗憾的是，虚对象包含指向虚函数指针表的指针,而write()将把这种信息复制到文件中。
+使用read()读取文件的内容，以填充对象时,函数指针值将为乱码，这将扰乱虚函数的行为)。
+可使用换行符将字段分隔开，这样在输入时将很容易识别各个字段。
+也可以使用二进制IO，但不能将对象作为一个整体写入，而应该提供分别对每个类成员应用write( )和read( )的类方法。这样，程序将只把所需的数据保存到文件中
+
+比较难处理的部分是使用文件恢复数据。
+问题在于:程序如何才能知道接下来要恢复的项目是cmployee对象、manager对象、fink对象还是 highfink对象?
+一种方法是，在对象的数据写入文件时，在数据前面加上一个指示对象类型的整数。
+这样，在文件输入时，程序便可以读取该整数，并使用switch语句创建-个适当的对象来接收数据:
+
+enum classkind{Employee,Manager，Fink，Highfink}; // in class header
+...
+int classtype;
+while((fin >> classtype).get (ch)){// newline separates int from data
+  switch(classtype) {
+    case Employee: pc[i] = new employee;
+                 : break;
+然后便可以使用指针调用虚函数getall()来读取信息:
+pc [i++] -> getal1();
 
 */
 #include "6.h"
+int main()
+{
+    
+}
