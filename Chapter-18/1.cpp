117e8fdeb499c929ff6876f7ab11aeca39d1cf23
WangYan-Good <987639823@qq.com>
2021-07-06 00:15:00 +0800

    C++_Chapter18/1

--- Chapter-18/1.cpp
+++ Chapter-18/1.cpp
@@ -1,1 +1,13 @@
 #include "1.h"
+#include <initializer_list>
+
+// template <typename T>
+// T average_list( std::initializer_list<T> list )
+// {
+//     T sum = 0;
+//     for( auto x:list )
+//     {
+//         sum += x;
+//     }
+//     return (T)(sum/list.size());
+// }
--- Chapter-18/1.h
+++ Chapter-18/1.h
@@ -1,6 +1,9 @@
 #ifndef _CHAPTER_18_1_H_
 #define _CHAPTER_18_1_H_
 
+#include <iostream>
 
+template <typename T>
+T average_list( std::initializer_list<T> list );
 
 #endif
