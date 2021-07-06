643071770b6df99aef469853db6eba299b921970
yanwa15x <yanx.wang@intel.com>
2021-07-06 15:22:25 +0800

    C++_Chapter18/3

--- Chapter-18/3-main.cpp
+++ Chapter-18/3-main.cpp
@@ -1,3 +1,27 @@
 /*
 3．编写并测试可变参数模板函数sum_value()，它接受任意长度的参数列表(其中包含数值，但可以是任何类型儿并以 long double的方式返回这些数值的租。
 */
+#include "3.h"
+
+template<typename T>
+T sum_value( std::initializer_list<T> It)
+{
+    T sum = 0;
+    for( auto x : It )
+    {
+        sum += x;
+    }
+    return sum;
+}
+
+int main()
+{
+    int sum_i = sum_value( { 1,2,3,4,5 } );
+    double sum_d = sum_value( { 1.1, 1.2, 1.3, 1.4 } );
+    float sum_f = sum_value( { 1.2, 1.3, 1.4, 1.5 } );
+
+    std::cout << "sum_i: " << sum_i << std::endl;
+    std::cout << "sum_f: " << sum_f << std::endl;
+    std::cout << "sum_d: " << sum_d << std::endl;
+    return 0;
+}
---
+++ Chapter-18/3.cpp
@@ -0,0 +1,12 @@
+#include "3.h"
+
+// template<typename T>
+// T sum_value( std::initializer_list<T> It)
+// {
+//     T sum = 0;
+//     for( auto x : It )
+//     {
+//         sum += x;
+//     }
+//     return x;
+// }
